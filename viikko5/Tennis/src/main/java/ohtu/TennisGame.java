c23fc1e4d5825f2d47fda412843c56de671cbe1d
aleksisv <aleksis.vuoksenmaa@helsinki.fi>
2017-04-22 16:06:17 +0300

    Tehtävä 5 tehty

--- viikko5/Tennis/src/main/java/ohtu/TennisGame.java
+++ viikko5/Tennis/src/main/java/ohtu/TennisGame.java
@@ -1,7 +1,7 @@
 package ohtu;
 
 public class TennisGame {
-    
+
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -22,57 +22,67 @@ public class TennisGame {
     public String getScore() {
         String score = "";
         int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
+        if (m_score1==m_score2) {
+            score = equalScore();
+        } else if (m_score1>=4 || m_score2>=4) {
+            score = advantageOrWin();
+        } else {
+            score = scoreInAnyOtherCase();
+        }
+        return score;
+    }
+
+    private String equalScore() {
+        String score = "";
+        switch (m_score1) {
+            case 0:
+                    score = "Love-All";
+                break;
+            case 1:
+                    score = "Fifteen-All";
+                break;
+            case 2:
+                    score = "Thirty-All";
+                break;
+            case 3:
+                    score = "Forty-All";
+                break;
+            default:
+                    score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private String advantageOrWin() {
+        String score = "";
+        int minusResult = m_score1-m_score2;
+        if (minusResult==1) score ="Advantage player1";
+        else if (minusResult ==-1) score ="Advantage player2";
+        else if (minusResult>=2) score = "Win for player1";
+        else score ="Win for player2";
+        return score;
+    }
+
+    private String scoreInAnyOtherCase() {
+        String score = "";
+        int tempScore = 0;
+        for (int i=1; i<3; i++) {
+            if (i==1) tempScore = m_score1;
+            else { score+="-"; tempScore = m_score2;}
+            switch(tempScore) {
                 case 0:
-                        score = "Love-All";
+                    score+="Love";
                     break;
                 case 1:
-                        score = "Fifteen-All";
+                    score+="Fifteen";
                     break;
                 case 2:
-                        score = "Thirty-All";
+                    score+="Thirty";
                     break;
                 case 3:
-                        score = "Forty-All";
+                    score+="Forty";
                     break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
-                }
             }
         }
         return score;
